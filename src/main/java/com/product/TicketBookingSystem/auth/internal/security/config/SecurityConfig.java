950262a757b0362b3e8be0c51cae00c162227fb9
r9988.2588 <r9988.2588@gmail.com>
2026-05-20 11:07:31 +0000

    feat(auth): add security config with filter chain and password encoder

---
+++ src/main/java/com/product/TicketBookingSystem/auth/internal/security/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.product.TicketBookingSystem.auth.internal.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                // authorizes request
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(
+                                "/auth/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs*/**",
+                                "/swagger-ui.html")
+                        .permitAll()
+                        .anyRequest().authenticated())
+
+                // enables basic auth
+                .httpBasic(Customizer.withDefaults())
+
+                // configures the session management to be stateless
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+    // BCrypt password encoder used to encode passwords
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
