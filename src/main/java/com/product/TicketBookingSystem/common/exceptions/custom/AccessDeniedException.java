0dabadd8b206950454abf9169768848214706736
r9988.2588 <r9988.2588@gmail.com>
2026-05-20 10:49:45 +0000

    feat(common): add common exceptions

---
+++ src/main/java/com/product/TicketBookingSystem/common/exceptions/custom/AccessDeniedException.java
@@ -0,0 +1,11 @@
+package com.product.TicketBookingSystem.common.exceptions.custom;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class AccessDeniedException extends RuntimeException {
+    public AccessDeniedException(String message) {
+        super(message);
+    }
+}
