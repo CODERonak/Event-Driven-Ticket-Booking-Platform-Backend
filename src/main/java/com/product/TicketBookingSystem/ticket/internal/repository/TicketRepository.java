e9650f04ab7cdbc4db47a0e9c9d39a6f387d5b8c
r9988.2588 <r9988.2588@gmail.com>
2026-05-28 08:44:33 +0000

    refactor(ticket): remove ticket module

--- src/main/java/com/product/TicketBookingSystem/ticket/internal/repository/TicketRepository.java
+++
@@ -1,9 +0,0 @@
-package com.product.TicketBookingSystem.ticket.internal.repository;
-
-import com.product.TicketBookingSystem.ticket.internal.model.entity.Ticket;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
